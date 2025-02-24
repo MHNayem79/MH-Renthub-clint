47d30721231da93c3910d29011f9e6212b18c373
MH Nayem <mhnayem79@gmail.com>
2025-02-24 18:40:50 +0600

    fixing some error of private routes

--- src/Router/PrivateRouter.jsx
+++ src/Router/PrivateRouter.jsx
@@ -3,6 +3,7 @@ import AuthContext from "../Context/AuthContext/AuthContext";
 import { Navigate, useLocation } from "react-router-dom";
 
 
+// eslint-disable-next-line react/prop-types
 const PrivateRouter = ({ children }) => {
     const { user, loading } = useContext(AuthContext)
     const location = useLocation()
